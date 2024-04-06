2185117741b316602fc54fcf8335883ab6d45913
AmrEmadEldeen <85315099+AmrEmadEldeen@users.noreply.github.com>
2024-04-07 01:13:27 +0200

    Time: 34 ms (50.63%), Space: 45.1 MB (8.9%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,17 @@
+
+import java.util.Stack;
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] out = new int[2];
+        for (int i = 0; i<nums.length; i++){
+            for (int j = i+1; j<nums.length; j++){
+                if((nums[i] + nums[j]) == target){
+                    out[0] = i;
+                    out[1] = j;
+                    break;
+                }
+            }
+        }
+      return out;
+}
+}
