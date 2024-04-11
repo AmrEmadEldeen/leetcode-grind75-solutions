5979886d4a519f0d8fc0138eec83cadf53f3b4fe
AmrEmadEldeen <85315099+AmrEmadEldeen@users.noreply.github.com>
2024-04-11 17:02:01 +0200

    Time: 127 ms (10.71%) | Memory: 45.8 MB (11.86%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,30 +1,41 @@
 class Solution {
     public boolean isPalindrome(String s) {
         boolean flag = true;
-        int c =0,i = 0, j;
-        
-        String s2 = s.replaceAll("[^a-zA-Z0-9]", "");
-        s2=s2.toLowerCase();
-        int size = s2.length();  
-        j = size -1 ;
-        
-      //  for (int i = 0; int j = (size -1); i< (size/2) ; j >= (size /2))
-      if (size%2==0){
-      while( i< (size/2) && j >= (size /2)){
-            if(s2.charAt(i) != s2.charAt(j)){
-                flag = false;
+        int c = 0, i = 0, j, len;
+        String chkStr = "";
+        // removing all non-alphanumeric characters.
+        while (c < s.length()) {
+            if ((s.charAt(c) >= 48 && s.charAt(c) <= 57) || (s.charAt(c) >= 65 && s.charAt(c) <= 90)
+                    || (s.charAt(c) >= 97 && s.charAt(c) <= 122)) {
+                chkStr = chkStr + s.charAt(c);
             }
-            i++;
-            j--;
-        }}
-        else{
-            while( i< (size/2) && j > (size /2)){
-            if(s2.charAt(i) != s2.charAt(j)){
-                flag = false;
+            c++;
+        }
+        // converting all uppercase letters into lowercase letters.
+        chkStr = chkStr.toLowerCase();
+        // length of the string after formatting.
+        len = chkStr.length();
+        j = len - 1;
+        // check if the length is even or odd then itirate from the begining and end of the string .
+        if (len % 2 == 0) {
+            while (i < (len / 2) && j >= (len / 2)) {
+                // compare between chars to validate Palindrome
+                if (chkStr.charAt(i) != chkStr.charAt(j)) {
+                    flag = false;
+                }
+                i++;
+                j--;
             }
-            i++;
-            j--;
-        }}
-    return flag;
+        } else {
+            while (i < (len / 2) && j > (len / 2)) {
+                
+                if (chkStr.charAt(i) != chkStr.charAt(j)) {
+                    flag = false;
+                }
+                i++;
+                j--;
+            }
+        }
+        return flag;
     }
 }
